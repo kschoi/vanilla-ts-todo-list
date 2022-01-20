54ff4d9f0594a2a7e2f1cc8fd6d61c8735b784e4
chloe <kschoi0308@gmail.com>
2022-01-20 15:32:20 +0900

    feat: render list

--- src/components/Todo.ts
+++ src/components/Todo.ts
@@ -1,15 +1,36 @@
 import { ITodoData } from "../entities";
 
 export default class Todo {
-  todo: any;
+  #target: HTMLElement;
+  #todo: HTMLElement;
+  #data: ITodoData;
 
-  constructor({ $target, data }: { $target: HTMLDivElement; data: ITodoData }) {
-    this.todo = document.createElement("article");
-    this.todo.className = "todo";
-    this.todo.dataset.id = data.id;
+  constructor(target: HTMLElement, data: ITodoData) {
+    this.#target = target;
+    this.#data = data;
+    this.#todo = document.createElement("article");
+    this.#todo.className = "todo";
+    this.#todo.dataset.id = `${data.id}`;
+    this.#todo.dataset.completed = `${data.completed}`;
 
-    $target.appendChild(this.todo);
+    this.render();
   }
 
-  render() {}
+  render() {
+    const id = `${this.#data.id}`;
+
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.id = id;
+    checkbox.readOnly = true;
+    checkbox.checked = this.#data.completed;
+
+    const label = document.createElement("label");
+    label.innerHTML = this.#data.title;
+    label.htmlFor = id;
+
+    this.#todo.appendChild(checkbox);
+    this.#todo.appendChild(label);
+    this.#target.appendChild(this.#todo);
+  }
 }
--- src/components/TodoList.ts
+++ src/components/TodoList.ts
@@ -1,32 +1,58 @@
 import { ITodoData } from "../entities";
+import Todo from "./Todo";
 
-export default class TodoList {
-  data: ITodoData[] | null;
-  container: any;
+interface ITodoList {
+  edit(e: Event): void;
+  delete(e: Event): void;
+  render(): void;
+}
+
+export default class TodoList implements ITodoList {
+  #data: ITodoData[] | null;
+  #container: any;
 
   constructor($target: HTMLDivElement, data: ITodoData[] | null) {
-    this.data = data;
-    this.container = document.createElement("section");
-    this.container.className = "todos";
-    $target.appendChild(this.container);
+    this.#data = data;
+    this.#container = document.createElement("section");
+    this.#container.className = "container";
+    this.#container.onclick = this.onClick.bind(this);
+
+    $target.appendChild(this.#container);
 
     this.render();
   }
 
-  render() {
-    this.container.innerHTML = "";
+  edit() {
+    alert("변경하기");
+  }
 
-    if (!this.data || this.data.length < 1) {
+  delete() {
+    alert("삭제하기");
+  }
+
+  onClick(event: Event) {
+    const target = event.target as HTMLButtonElement;
+    let action = target.dataset.action as keyof ITodoList;
+    if (action && this[action]) {
+      this[action]();
+    }
+  }
+
+  render() {
+    this.#container.innerHTML = "";
+
+    if (!this.#data || this.#data.length < 1) {
       const listPlaceholder = document.createElement("div");
       listPlaceholder.innerHTML = "<p>할일을 등록해주세요.</p>";
-      listPlaceholder.className = "list-placeholder";
-      this.container.appendChild(listPlaceholder);
+      listPlaceholder.className = "todo-list-placeholder";
+      this.#container.appendChild(listPlaceholder);
 
       return;
     }
 
-    const list = document.createElement("ul");
-    list.innerHTML = JSON.stringify(this.data);
-    this.container.appendChild(list);
+    const list = document.createElement("section");
+    list.className = "todo-list";
+    this.#data.forEach((data) => new Todo(list, data));
+    this.#container.appendChild(list);
   }
 }
